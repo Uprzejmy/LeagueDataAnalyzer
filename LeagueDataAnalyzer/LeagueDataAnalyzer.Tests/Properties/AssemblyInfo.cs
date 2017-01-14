d98e67894325ffcb0f636c679fd7e17460149ed1
Uprzejmy <lmerynda@gmail.com>
2017-01-14 23:49:19 +0100

    Project changed to ASP.NET app

--- LeagueDataAnalyzer/LeagueDataAnalyzer.Tests/Properties/AssemblyInfo.cs
+++ LeagueDataAnalyzer/LeagueDataAnalyzer.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LeagueDataAnalyzer")]
+[assembly: AssemblyTitle("LeagueDataAnalyzer.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LeagueDataAnalyzer")]
+[assembly: AssemblyProduct("LeagueDataAnalyzer.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1dc36f44-6222-4822-b591-1c5ca2c83b51")]
+[assembly: Guid("a2e0b4ec-0200-4ff5-98b0-bb8c94497c7a")]
 
 // Version information for an assembly consists of the following four values:
 //
